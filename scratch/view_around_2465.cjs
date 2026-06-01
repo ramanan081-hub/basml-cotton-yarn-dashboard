d7a048c4b3354e92399b965de22d6539f4c013f6
BASML Developer <procurement@basml.com>
2026-06-01 11:27:46 +0530

    Clean up scratch files

--- scratch/view_around_2465.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('src/App.jsx', 'utf8');
-const lines = code.split('\n');
-
-for (let i = 2400 - 1; i < 2470; i++) {
-  if (i < lines.length) {
-    console.log(`${i + 1}: ${lines[i]}`);
-  }
-}
